5ec15e54deba3c111b9bce63ade5c98fd736110b
BuddyActivist <buddyactivist@gmail.com>
2026-01-18 18:17:02 +0100

    Add BuddyPress component checks
    Added functions to check BuddyPress xProfile and avatar availability.

--- includes/bapl-helpers.php
+++ includes/bapl-helpers.php
@@ -12,8 +12,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Get the URL of a page by its slug.
  *
- * Falls back to home_url( /slug/ ) if the page object cannot be found.
- *
  * @param string $slug Page slug.
  * @return string URL.
  */
@@ -22,7 +20,6 @@ function bapl_get_page_url( $slug ) {
     if ( $page ) {
         return get_permalink( $page->ID );
     }
-
     return home_url( '/' . $slug . '/' );
 }
 
@@ -34,4 +31,22 @@ function bapl_get_page_url( $slug ) {
 function bapl_redirect( $url ) {
     wp_safe_redirect( $url );
     exit;
+}
+
+/**
+ * Check if BuddyPress xProfile component is active.
+ *
+ * @return bool
+ */
+function bapl_is_bp_xprofile_active() {
+    return function_exists( 'bp_is_active' ) && bp_is_active( 'xprofile' );
+}
+
+/**
+ * Check if BuddyPress avatar functions are available.
+ *
+ * @return bool
+ */
+function bapl_is_bp_avatar_active() {
+    return function_exists( 'bp_core_avatar_handle_upload' );
 }
