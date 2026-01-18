73596faa3f7e8ea745f5250fe143858a019e6a75
BuddyActivist <buddyactivist@gmail.com>
2026-01-18 19:42:55 +0100

    Create class-bapl-cron.php

---
+++ includes/class-bapl-cron.php
@@ -0,0 +1,47 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class BAPL_Cron {
+
+    public function init() {
+        add_action( 'bapl_check_unpaid_users', [ $this, 'check_unpaid_users' ] );
+
+        if ( ! wp_next_scheduled( 'bapl_check_unpaid_users' ) ) {
+            wp_schedule_event( time(), 'quarterhourly', 'bapl_check_unpaid_users' );
+        }
+
+        add_filter( 'cron_schedules', [ $this, 'add_quarterhourly_schedule' ] );
+    }
+
+    public function add_quarterhourly_schedule( $schedules ) {
+        $schedules['quarterhourly'] = [
+            'interval' => 15 * 60,
+            'display'  => 'Every 15 minutes'
+        ];
+        return $schedules;
+    }
+
+    public function check_unpaid_users() {
+
+        $args = [
+            'meta_key'     => 'bapl_payment_timestamp',
+            'meta_compare' => 'EXISTS',
+            'fields'       => 'ID'
+        ];
+
+        $users = get_users( $args );
+
+        foreach ( $users as $user_id ) {
+
+            $timestamp = (int) get_user_meta( $user_id, 'bapl_payment_timestamp', true );
+            $completed = get_user_meta( $user_id, 'bapl_payment_completed', true );
+
+            if ( $completed ) continue;
+
+            if ( time() > $timestamp + 3600 ) {
+                require_once ABSPATH . 'wp-admin/includes/user.php';
+                wp_delete_user( $user_id );
+            }
+        }
+    }
+}
